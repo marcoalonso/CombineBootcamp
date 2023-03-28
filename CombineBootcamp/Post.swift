fc0488e6561263b9f895c0c31ff634a0b6fd0cf2
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-03-28 12:33:31 -0600

    Sending data from api to TableView with combine

---
+++ CombineBootcamp/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  CombineBootcamp
+//
+//  Created by Marco Alonso Rodriguez on 28/03/23.
+//
+
+import Foundation
+
+
+struct Post : Codable {
+    let title: String
+    let body: String
+}
---
+++ CombineBootcamp/Webservice.swift
@@ -0,0 +1,26 @@
+//
+//  Webservice.swift
+//  CombineBootcamp
+//
+//  Created by Marco Alonso Rodriguez on 28/03/23.
+//
+
+import Foundation
+import Combine
+
+class Webservice {
+    
+    
+    func getPosts() -> AnyPublisher<[Post], Error> {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            fatalError("Invalid URL")
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: [Post].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
