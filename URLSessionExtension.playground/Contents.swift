804038306ca446c80c656590bfe8f3b4cb5fa4c5
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-03-28 11:55:52 -0600

    Returning Post struct with combine

--- URLSessionExtension.playground/Contents.swift
+++ URLSessionExtension.playground/Contents.swift
@@ -1,12 +1,18 @@
 import Cocoa
 import Combine
 
-func getPosts() -> AnyPublisher<Data, URLError> {
+struct Post : Codable {
+    let title: String
+    let body: String
+}
+
+func getPosts() -> AnyPublisher<[Post], Error> {
     guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
         fatalError("Invalid URL")
     }
     
     return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }
+        .decode(type: [Post].self, decoder: JSONDecoder())
         .eraseToAnyPublisher()
 }
 
